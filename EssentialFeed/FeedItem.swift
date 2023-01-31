c5ccf72330a5d7807b6ff8ab7987d77a0e1a36c1
noivo <jmnoivo@gmail.com>
2023-01-31 16:39:40 +0000

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by João Noivo on 31/01/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let imageURL: URL
+}
