1e8738022510facfd56c6f73575285b07a15ff4f
noivo <jmnoivo@gmail.com>
2023-02-03 09:51:10 +0000

    Remove the generic error type form the `LoadFeedResult` to simplify type constraints

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -45,7 +45,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     let (sut, client) = makeSUT()
     
     // Act - When we tell the sut to load and we complete the client's HTTP request with an error
-    expect(sut, toCompleteWith: .failure(.connectivity)) {
+    expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity)) {
       let clientError = NSError(domain: "Test", code: 0)
       client.complete(with: clientError)
     }
@@ -60,7 +60,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     let samples =  [199, 201, 300, 400, 500]
     
     samples.enumerated().forEach { index, code in
-      expect(sut, toCompleteWith: .failure(.invalidData)) {
+      expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
         let json = makeItemsJSON([])
         client.complete(withStatusCode: code, data: json, at: index)
       }
@@ -70,7 +70,7 @@ class RemoteFeedLoaderTests: XCTestCase {
   func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
     let (sut, client) = makeSUT()
     
-    expect(sut, toCompleteWith: .failure(.invalidData)) {
+    expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
       let invalidJSON = Data("invalid json".utf8)
       client.complete(withStatusCode: 200, data: invalidJSON)
     }
@@ -183,7 +183,7 @@ class RemoteFeedLoaderTests: XCTestCase {
       case let (.success(receivedItems), .success(expectedItems)):
         XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
       
-      case let (.failure(receivedError), .failure(expectedError)):
+      case let (.failure(receivedError as RemoteFeedLoader.Error), .failure(expectedError as RemoteFeedLoader.Error)):
         XCTAssertEqual(receivedError, expectedError, file: file, line: line)
         
       default:
