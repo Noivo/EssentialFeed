36ebbb85f1afab21a9a93368220b8bace5aa7172
noivo <jmnoivo@gmail.com>
2023-02-15 17:06:59 +0000

    `CoreDataFeedStore.insert()` overrides previously inserted cache values

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -44,7 +44,7 @@ public final class CoreDataFeedStore: FeedStore {
     let context = self.context
         context.perform {
           do {
-            let managedCache = ManagedCache(context: context)
+            let managedCache = try ManagedCache.newUniqueInstance(in: context)
             managedCache.timestamp = timestamp
             managedCache.feed = NSOrderedSet(array: feed.map { local in
               let managed = ManagedFeedImage(context: context)
@@ -104,6 +104,21 @@ private extension NSManagedObjectModel {
 private class ManagedCache: NSManagedObject {
   @NSManaged var timestamp: Date
   @NSManaged var feed: NSOrderedSet
+  
+  static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+      let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+      request.returnsObjectsAsFaults = false
+      return try context.fetch(request).first
+    }
+
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+      try find(in: context).map(context.delete)
+      return ManagedCache(context: context)
+    }
+
+    var localFeed: [LocalFeedImage] {
+      return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+    }
 }
 
 @objc(ManagedFeedImage)
@@ -113,4 +128,19 @@ private class ManagedFeedImage: NSManagedObject {
   @NSManaged var location: String?
   @NSManaged var url: URL
   @NSManaged var cache: ManagedCache
+  
+  static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+    return NSOrderedSet(array: localFeed.map { local in
+      let managed = ManagedFeedImage(context: context)
+      managed.id = local.id
+      managed.imageDescription = local.description
+      managed.location = local.location
+      managed.url = local.url
+      return managed
+    })
+  }
+  
+  var local: LocalFeedImage {
+    return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+  }
 }
