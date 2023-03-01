62d6d68d410c8063415206f421c563c7098272aa
noivo <jmnoivo@gmail.com>
2023-03-01 22:14:21 +0000

    Move `MainQueueDispatchDecorator` to separate file

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -10,7 +10,7 @@ import EssentialFeed
 
 public final class FeedUIComposer {
   private init() {}
-
+  
   public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
     let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: MainQueueDispatchDecorator(decoratee: feedLoader))
     
@@ -27,38 +27,6 @@ public final class FeedUIComposer {
       loadingView: WeakRefVirtualProxy(feedController)
     )
     return feedController
-  }
-}
-
-private final class MainQueueDispatchDecorator<T> {
-  private let decoratee: T
-  
-  init(decoratee: T) {
-    self.decoratee = decoratee
-  }
-  
-  func dispatch(completion: @escaping () -> Void) {
-    guard Thread.isMainThread else {
-      return DispatchQueue.main.async(execute: completion)
-    }
-    
-    completion()
-  }
-}
-
-extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
-  func load(completion: @escaping (FeedLoader.Result) -> Void) {
-    decoratee.load { [weak self] result in
-      self?.dispatch { completion(result) }
-    }
-  }
-}
-
-extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
-  func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-    return decoratee.loadImageData(from: url) { [weak self] result in
-      self?.dispatch { completion(result) }
-    }
   }
 }
 
