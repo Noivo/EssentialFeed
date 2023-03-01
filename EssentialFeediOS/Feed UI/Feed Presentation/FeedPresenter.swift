84efacb5835541b7158742a8d04bf095750cbe92
noivo <jmnoivo@gmail.com>
2023-03-01 14:43:44 +0000

    Localize feed view title string

--- EssentialFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedPresenter.swift
@@ -5,6 +5,7 @@
 //  Created by João Noivo on 27/02/2023.
 //
 
+import Foundation
 import EssentialFeed
 
 struct FeedLoadingViewModel {
@@ -33,7 +34,11 @@ final class FeedPresenter {
   }
   
   static var title: String {
-    return "My Feed"
+    return NSLocalizedString("FEED_VIEW_TITLE",
+     tableName: "Feed",
+     bundle: Bundle(for: FeedPresenter.self),
+     comment: "Title for the feed view"
+    )
   }
   
   func didStartLoadingFeed() {
