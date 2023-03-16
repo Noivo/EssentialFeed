3c4a3b0c3673d3718fffb984e70f8303a94c0d84
noivo <jmnoivo@gmail.com>
2023-03-16 13:20:22 +0000

    Move `FeedErrorViewModel` to sparate file

---
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by João Noivo on 16/03/2023.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+  let message: String?
+}
