271436188330ed4579991e65c3b3e0a6bf77f44d
noivo <jmnoivo@gmail.com>
2023-03-16 14:28:09 +0000

    Move `FeedViewModel` and `FeedLoadingViewModel` to owns file

---
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by João Noivo on 16/03/2023.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+  let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by João Noivo on 16/03/2023.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+  let feed: [FeedImage]
+}
