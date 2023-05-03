c96c8a601b1152885a696da5881ce233725831ec
Miguel Noivo <jmnoivo@gmail.com>
2023-05-03 13:58:25 +0100

    Render comments

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -12,25 +12,59 @@ extension ListViewController {
     
     public override func loadViewIfNeeded() {
         super.loadViewIfNeeded()
-
+        
         tableView.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
     }
-
-  func simulateUserInitiatedReload() {
-    refreshControl?.simulatePullToRefresh()
-  }
+    
+    func simulateUserInitiatedReload() {
+        refreshControl?.simulatePullToRefresh()
+    }
     
     var isShowingLoadingIndicator: Bool {
         return refreshControl?.isRefreshing == true
     }
-
+    
     func simulateErrorViewTap() {
         errorView.simulateTap()
     }
-
+    
     var errorMessage: String? {
         return errorView.message
     }
+}
+
+extension ListViewController {
+    func numberOfRenderedComments() -> Int {
+        tableView.numberOfSections == 0 ? 0 :  tableView.numberOfRows(inSection: commentsSection)
+    }
+
+    func commentMessage(at row: Int) -> String? {
+        commentView(at: row)?.messageLabel.text
+    }
+
+    func commentDate(at row: Int) -> String? {
+        commentView(at: row)?.dateLabel.text
+    }
+
+    func commentUsername(at row: Int) -> String? {
+        commentView(at: row)?.usernameLabel.text
+    }
+
+    private func commentView(at row: Int) -> ImageCommentCell? {
+        guard numberOfRenderedComments() > row else {
+            return nil
+        }
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: commentsSection)
+        return ds?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+    }
+
+    private var commentsSection: Int {
+        return 0
+    }
+}
+
+extension ListViewController {
   
   @discardableResult
   func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
