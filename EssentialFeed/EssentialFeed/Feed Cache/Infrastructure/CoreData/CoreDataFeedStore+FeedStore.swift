043dadea9a7d18af041f47d574a302e4736be246
Miguel Noivo <jmnoivo@gmail.com>
2023-05-17 15:21:53 +0100

    Rename method to clarify intent

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -10,32 +10,32 @@ import CoreData
 extension CoreDataFeedStore: FeedStore {
 
   public func retrieve(completion: @escaping RetrievalCompletion) {
-    perform { context in
-      completion(Result {
-        try ManagedCache.find(in: context).map {
-          CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
-        }
-      })
-    }
+      performAsync { context in
+          completion(Result {
+            try ManagedCache.find(in: context).map {
+              CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
+            }
+          })
+      }
   }
 
   public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-    perform { context in
-      completion(Result {
-        let managedCache = try ManagedCache.newUniqueInstance(in: context)
-        managedCache.timestamp = timestamp
-        managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
-        try context.save()
-      })
-    }
+      performAsync { context in
+          completion(Result {
+            let managedCache = try ManagedCache.newUniqueInstance(in: context)
+            managedCache.timestamp = timestamp
+            managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
+            try context.save()
+          })
+      }
   }
 
   public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-    perform { context in
-      completion(Result {
-        try ManagedCache.deleteCache(in: context)
-      })
-    }
+      performAsync { context in
+          completion(Result {
+            try ManagedCache.deleteCache(in: context)
+          })
+      }
   }
 
 }
